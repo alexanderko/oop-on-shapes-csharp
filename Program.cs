e35b2b7ab6a40b256fa4aac9f4d61102e394e61a
Alexander Kotov <alexanderko@interlink-ua.com>
2020-08-05 15:10:30 +0300

    Add shape class

--- Program.cs
+++ Program.cs
@@ -2,11 +2,34 @@
 
 namespace oop_on_shapes
 {
+    class Shape 
+    {
+        char color;
+
+        public Shape(char color)
+        {
+            this.color = color;
+        }
+
+        public override string ToString()
+        {
+            return $"Shape of color {color}";
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Shape[] shapes = {
+                new Shape('r'),
+                new Shape('b'),
+                new Shape('w')
+            };
+
+            foreach(var shape in shapes)
+            {
+                Console.WriteLine(shape);
+            }
         }
     }
 }
