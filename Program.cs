2b42c8876f74b2fc6e90e2354961daf51484b548
Alexander Kotov <alexanderko@interlink-ua.com>
2020-08-05 15:30:33 +0300

    Implement Circle

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 
 namespace oop_on_shapes
 {
-    class Shape 
+    abstract class Shape 
     {
         char color;
 
@@ -15,21 +15,51 @@ namespace oop_on_shapes
         {
             return $"Shape of color {color}";
         }
+
+        abstract public double CalculateArea();
+    }
+
+    class Circle : Shape
+    {
+        int radius;
+
+        public Circle(char color, int radius) : base(color)
+        {
+            this.radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * radius * radius;
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
             Shape[] shapes = {
-                new Shape('r'),
-                new Shape('b'),
-                new Shape('w')
+                new Circle('r', 3),
+                new Circle('b', 3),
+                new Circle('w', 3)
             };
 
             foreach(var shape in shapes)
             {
                 Console.WriteLine(shape);
             }
+
+            double area = GetTotalArea(shapes);
+            Console.WriteLine("Area: {0}", area);
+        }
+
+        private static double GetTotalArea(Shape[] shapes)
+        {
+            double area = 0;
+            foreach(var shape in shapes)
+            {
+                area += shape.CalculateArea();
+            }
+            return area;
         }
     }
 }
