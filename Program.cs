9c11267ded6bb8235d3f1e81cf30e9b5220995d4
Alexander Kotov <alexanderko@interlink-ua.com>
2020-08-05 14:30:42 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oop_on_shapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
